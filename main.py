c7791873b1f715cd8d66d29bf26e9f1ce31d2ab9
bhumika503 <bhumika.lamba5sg@gmail.com>
2022-07-06 17:42:26 +0530

    refactor and comments

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import numpy as np
 
-
 from traverse import Traverse
 
 
-def interaction(incoming, present):
-    if incoming == 2 and present == 1:
-        present = 2
-    else:
-        present
-    return present
+def interaction(source, sink):
+    # interaction b/w source ward and sink ward
+    if source == 2 and sink == 1:
+        sink = 2
+    else: # no interaction when sink == 2 | sink == 0
+        sink
+    return sink
 
 def action(A, y, x, B):
-    incoming = A[y][x]
-    if incoming == 2:
-        valid_directions = [(y_next, x_next) for (y_next,x_next) in [Traverse.top(y,x,M,N), Traverse.right(y,x,M,N), Traverse.below(y,x,M,N),Traverse.left(y,x,M,N)] if (y_next, x_next) != (y, x)]
-        for (y_next,x_next) in valid_directions:
 
-            present = A[y_next][x_next]
-            B[y_next][x_next] = interaction(incoming, present)
-    else:
+    # identify source
+    source = A[y][x]
+
+    # virus only spreads from source == 2
+    if source == 2:
+        valid_directions = Traverse.valid_traversals(y,x,M,N)
+        for (y_next,x_next) in valid_directions:
+            sink = A[y_next][x_next]
+            B[y_next][x_next] = interaction(source, sink)
+    else: # alternate source
         pass
 
     return B
@@ -27,7 +30,6 @@ def action(A, y, x, B):
 def execute_one_time_unit(A, M, N, B):
     for y in range(M):
         for x in range(N):
-            # new_matrix is only used for the update
             B = action(A, y, x, B)
     return B
 
@@ -59,6 +61,8 @@ if __name__ == '__main__':
     # B = [[2,1,0,2,1],[1,1,1,1,1],[1,0,0,2,1]]
 
     A = np.random.randint(3, size=(1000, 1000))
+
+    # new_matrix is only used for the update
     B = A.copy()
 
     M,N = np.shape(A)
--- traverse.py
+++ traverse.py
@@ -3,6 +3,7 @@ class Traverse:
     # traverse in top,right,bottom,left direction
     # relative to current position in 2D Array
     def top(y,x,M,N):
+        # Prevent top traversals for first row
         if y == 0:
             y
         else:
@@ -10,6 +11,7 @@ class Traverse:
         return y, x
 
     def below(y,x,M,N):
+        # Prevent the bottom traversals for last row
         if y == M-1:
             y
         else:
@@ -17,6 +19,7 @@ class Traverse:
         return y,x
 
     def left (y,x,M,N):
+        # Prevent left traversals for first column
         if x == 0:
             x
         else:
@@ -24,8 +27,14 @@ class Traverse:
         return y, x
 
     def right(y,x,M,N):
+        # Prevent the right traversals for last column
         if x == N-1:
             x
         else:
             x = x+1
         return y, x
+
+    def valid_traversals(y,x,M,N):
+
+        # exclude the traversals which result in same coordinates as starting point
+        return [(y_next, x_next) for (y_next,x_next) in [Traverse.top(y,x,M,N), Traverse.right(y,x,M,N), Traverse.below(y,x,M,N),Traverse.left(y,x,M,N)] if (y_next, x_next) != (y, x)]
