1ce6e2d5fb6eafed6610646d1e7b50c199d5ea5e
bhumika503 <bhumika.lamba5sg@gmail.com>
2022-07-06 21:32:47 +0530

    added comments

--- main.py
+++ main.py
@@ -3,20 +3,21 @@ import numpy as np
 from model_virus import ModelVirusSpread
 
 
-def ultimate(A,M,N,B):
+def calculate_min_time(A,M,N,B):
+
     time_counter = 0
     while True:
         if list(zip(*np.where(np.array(B) == 1))):
-            # if any unifected ward remains
+        # if any unifected ward remains
 
             B, count = ModelVirusSpread.execute_one_time_unit(A,M,N,B)
 
             time_counter += 1
-            print('count', count)
-            print(time_counter)
+            # print('count', count)
+            # print(time_counter)
 
             if count == 0:
-                # count 0 -> if no change in A and B after interaction
+            # count 0 -> if no change in A and B after interaction
                 return -1
             else:
                 # Update the state after one time unit
@@ -26,19 +27,30 @@ def ultimate(A,M,N,B):
         else:
             return time_counter
 
-# m = 3 # no: of rows
-# n = 4 # no: of columns
-if __name__ == '__main__':
-    # A = [[2,1,0,2,1],[1,1,1,1,1],[1,0,0,2,1]]
-    # B = [[2,1,0,2,1],[1,1,1,1,1],[1,0,0,2,1]]
 
-    # A: Original state
-    A = np.random.randint(3, size=(1000, 1000))
+if __name__ == '__main__':
+
+    # Input the rows and columns of the hospital
+    # Example Input: 3,3
+    r, c = input('Input # rows and #columns :').split(',')
+
+    rows = int(r)
+    columns = int(c)
+
+    # A: 2-D array
+    # A: Initial State
+    # Random matrix is constructed
+
+    A = np.random.randint(3, size=(rows, columns))
 
     # B: Transient state used to update the matrix after virus interactions.
     B = A.copy()
 
     M,N = np.shape(A)
 
-    output = ultimate(A,M,N,B)
+    # Input from pdf
+    # A = [[2,1,0,2,1],[1,1,1,1,1],[1,0,0,2,1]]
+    # B = [[2,1,0,2,1],[1,1,1,1,1],[1,0,0,2,1]]
+
+    output = calculate_min_time(A,M,N,B)
     print(output)
